656555f1916de5437bd0820336272f0cbd14e4bb
danieljohannsson <daniel.johannsson1999@gmail.com>
2024-10-20 17:38:07 +0200

    Added Chakra UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 // /src/App.tsx
 import React from 'react';
 import JobSearchPage from './pages/JobSearchPage';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App: React.FC = () => {
     return (
+        <ChakraProvider>
         <div className="app">
             <JobSearchPage />
         </div>
+        </ChakraProvider>
     );
 };
 
