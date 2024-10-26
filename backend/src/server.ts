2a356084c5cda43384fd24306eb018f909f2cc5b
danieljohannsson <daniel.johannsson1999@gmail.com>
2024-10-26 13:27:32 +0200

    Added sqlite3 database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,15 @@
 // server.ts
 import app from './app';
+import { initDb } from './database';
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+// Initialize the database and then start the server
+initDb().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  });
+}).catch((err) => {
+  console.error('Failed to initialize the database', err);
 });
