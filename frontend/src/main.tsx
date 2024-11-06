3e723833f7064bde8a5b8f67917e0337ddf886b7
Daníel Jóhannsson <daniel.johannsson1999@gmail.com>
2024-11-06 11:52:23 +0100

    Removed Eslint comment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css'; // Optional: Global styles
 import App from './App';
 
-// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
   <React.StrictMode>
     <App />
