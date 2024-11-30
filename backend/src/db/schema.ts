79776238152643e49d56cb25b6a08a7178435175
Daníel Jóhannsson <daniel.johannsson1999@gmail.com>
2024-11-30 14:53:50 +0100

    Updated db-schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,54 +1,59 @@
 import { sql } from "drizzle-orm";
-import { sqliteTable, text, integer, primaryKey
+import { sqliteTable, text, integer, primaryKey, check
  } from "drizzle-orm/sqlite-core";
+ import { v4 as uuidv4 } from 'uuid';
 
-export const users = sqliteTable('users', {
-  userId: integer('user_id').primaryKey({autoIncrement: true}),
+;
+
+export const usersTable = sqliteTable('users', {
+  userId: text('user_id').primaryKey().$defaultFn(() => uuidv4()),
   email: text('email').notNull().unique(),
-  createdAt: text('created_at').default('CURRENT_TIMESTAMP'),
-});
+  createdAt: text('created_at').default(sql`(datetime('now', 'localtime'))`),
+},(table) => ({
+  checkConstraint: check("email_check", sql`email LIKE '%@%.%'`),
+}));
 
-export const roles = sqliteTable('roles', {
-  roleId: integer('role_id').primaryKey({autoIncrement: true}),
+export const rolesTable = sqliteTable('roles', {
+  roleId: text('role_id').primaryKey().$defaultFn(() => uuidv4()),
   roleName: text('role_name').notNull().unique(),
 });
 
-export const locations = sqliteTable('locations', {
-  locationId: integer('location_id').primaryKey({autoIncrement: true}),
+export const locationsTable = sqliteTable('locations', {
+  locationId: text('location_id').primaryKey().$defaultFn(() => uuidv4()),
   locationName: text('location_name').notNull().unique(),
 });
 
-export const companies = sqliteTable('companies', {
-  companyId: integer('company_id').primaryKey({autoIncrement: true}),
+export const companiesTable = sqliteTable('companies', {
+  companyId: text('company_id').primaryKey().$defaultFn(() => uuidv4()),
   companyName: text('company_name').notNull().unique(),
 });
 
 
-export const userPreferences = sqliteTable('user_preferences', {
-  preferenceId: integer('preference_id').primaryKey({autoIncrement: true}),
-  userId: integer('user_id').references(() => users.userId, { onDelete: 'cascade' }).notNull(),
-  roleId: integer('role_id').references(() => roles.roleId),
-  locationId: integer('location_id').references(() => locations.locationId),
-  companyId: integer('company_id').references(() => companies.companyId),
+export const userPreferencesTable = sqliteTable('user_preferences', {
+  preferenceId: text('preference_id').primaryKey().$defaultFn(() => uuidv4()),
+  userId: text('user_id').references(() => usersTable.userId, { onDelete: 'cascade' }).notNull(),
+  roleId: text('role_id').references(() => rolesTable.roleId),
+  locationId: text('location_id').references(() => locationsTable.locationId),
+  companyId: text('company_id').references(() => companiesTable.companyId),
 }
 );
 
-export const jobs = sqliteTable('jobs', {
-  jobId: integer('job_id').primaryKey({autoIncrement: true}),
+export const jobsTable = sqliteTable('jobs', {
+  jobId: text('job_id').primaryKey().$defaultFn(() => uuidv4()),
   title: text('title').notNull(),
   companyName: text('company_name'),
   location: text('location'),
   role: text('role'),
   url: text('url'), // Link to the job posting
-  createdAt: text('created_at').default('CURRENT_TIMESTAMP'),
+  createdAt: text('created_at').default(sql`(datetime('now', 'localtime'))`),
 });
 
-export const jobRecommendations = sqliteTable('job_recommendations', {
-  recommendationId: integer('recommendation_id').primaryKey({autoIncrement: true}),
-  sentAt: text('sent_at').default('CURRENT_TIMESTAMP'),
-  userId: integer("user_id")
-    .references(() => users.userId, { onDelete: 'cascade' }) // Set up the foreign key reference
+export const jobRecommendationsTable = sqliteTable('job_recommendations', {
+  recommendationId: text('recommendation_id').primaryKey().$defaultFn(() => uuidv4()),
+  sentAt: text('sent_at').default(sql`(datetime('now', 'localtime'))`),
+  userId: text("user_id")
+    .references(() => usersTable.userId, { onDelete: 'cascade' }) // Set up the foreign key reference
     .notNull(),
-  jobId: integer("job_id").references(() => jobs.jobId).notNull(),
+  jobId: text("job_id").references(() => jobsTable.jobId).notNull(),
 }
 );
